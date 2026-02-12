477732de1e6345a08ad623ec80a7a6ee7e45d277
kadamt283-rgb <kadamt283@gmail.com>
2026-02-12 13:17:14 +0530

    fix: remove nested git repo in frontend and add files

---
+++ frontend/public/js/main.js
@@ -0,0 +1,67 @@
+(function ($) {
+    "use strict";
+
+    
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+
+
+   
+    new WOW().init();
+
+
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.nav-bar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+
+
+
+   
+    $('.quantity button').on('click', function () {
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
+
+
+   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+
+
+})(jQuery);
+
